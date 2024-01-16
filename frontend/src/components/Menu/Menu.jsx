0e95c8b6660d90e75c0011cd5770f53f96a03734
Jonasz Janicki <34692088+PaperManV2@users.noreply.github.com>
2024-01-16 18:12:40 +0100

    Alabama is Good country... I think

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,65 +1,114 @@
-import React, { useState } from "react";
+import React from "react";
 import css from "./Menu.module.css";
 import icon from "../../icons/icons.svg";
 
 const Menu = ({ open, onClose }) => {
-  const [tooltipContent, setTooltipContent] = useState(null);
-  const handleIconClick = (content) => {
-    setTooltipContent(content);
-  };
   return (
     <>
       {
         <>
           <div className={`${css.menu} ${open ? css.open : css.close}`}>
-            <div className={css.arrow}>
-              <div className={css.arrow_icon}></div>
-              <div className={css.arrow_icon}></div>
-              <div className={css.arrow_icon}></div>
+            <div className={css.menuHeader}>
+              <svg
+                className={`${css.arrow_right}`}
+                width="36px"
+                height="32px"
+                fill="white"
+                onClick={onClose}
+              >
+                <use href={`${icon}#arrow_right`}></use>
+              </svg>
+              <div className={css.companyInfo}>
+                <div className={css.logo}>
+                  <svg className={`${css.logo_icon}`}>
+                    <use href={`${icon}#logo`}></use>
+                  </svg>
+                </div>
+                <span className={css.title}>Podłącz się do świata!</span>
+              </div>
             </div>
+
             <ul>
-              <li>Karmel</li>
-              <li>Ciastko</li>
-              <li>Czekolada</li>
-              <li>Żwirek</li>
-              <li>Muchomorek</li>
-              <li>Ciastek</li>
-            </ul>
-            <ul>
-              <li
-                className={css.iconWithTooltip}
-                onClick={() => handleIconClick("example@email.com")}
-              >
-                <svg
-                  className={`${css.envelop}`}
-                  width="48px"
-                  height="32px"
-                  fill="white"
-                >
-                  <use href={`${icon}#envelop`}></use>
-                </svg>
+              <li>
+                <a /*href="#" */>
+                  <svg
+                    className={`${css.house}`}
+                    width="48px"
+                    height="32px"
+                    fill="white"
+                  >
+                    <use href={`${icon}#house`}></use>
+                  </svg>
+                  <span>Home</span>
+                </a>
               </li>
-              <li
-                className={css.iconWithTooltip}
-                onClick={() => handleIconClick("123-456-7890")}
-              >
-                <svg
-                  className={`${css.phone}`}
-                  width="48px"
-                  height="32px"
-                  fill="white"
-                >
-                  <use href={`${icon}#phone`}></use>
-                </svg>
+              <li>
+                <a /*href="#" */>
+                  <svg
+                    className={`${css.account}`}
+                    width="48px"
+                    height="32px"
+                    fill="white"
+                  >
+                    <use href={`${icon}#account`}></use>
+                  </svg>
+                  <span>Login</span>
+                </a>
+              </li>
+              <li>
+                <a /*href="#" */>
+                  <svg
+                    className={`${css.groupPeople}`}
+                    width="48px"
+                    height="32px"
+                    fill="white"
+                  >
+                    <use href={`${icon}#groupPeople`}></use>
+                  </svg>
+                  <span>O nas</span>
+                </a>
+              </li>
+              <li>
+                <a /*href="#" */>
+                  <svg
+                    className={`${css.news}`}
+                    width="48px"
+                    height="32px"
+                    fill="white"
+                  >
+                    <use href={`${icon}#news`}></use>
+                  </svg>
+                  <span>Newsy</span>
+                </a>
+              </li>
+              <li>
+                <a /*href="#" */>
+                  <svg
+                    className={`${css.map}`}
+                    width="48px"
+                    height="32px"
+                    fill="white"
+                  >
+                    <use href={`${icon}#map`}></use>
+                  </svg>
+                  <span>Lokalizacja</span>
+                </a>
+              </li>
+              <li>
+                <a /*href="#" */>
+                  <svg
+                    className={`${css.pen}`}
+                    width="48px"
+                    height="32px"
+                    fill="white"
+                  >
+                    <use href={`${icon}#pen`}></use>
+                  </svg>
+                  <span>Zgłoszenia</span>
+                </a>
               </li>
             </ul>
           </div>
-          {tooltipContent && (
-            <div className={css.tooltip}>
-              {tooltipContent}
-              <span onClick={() => setTooltipContent(null)}>X</span>
-            </div>
-          )}
         </>
       }
     </>
