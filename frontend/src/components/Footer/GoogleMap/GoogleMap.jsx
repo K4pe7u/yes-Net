2e0aedcccba2e42c7ed35dcca9192c397a69ddff
Juliusz Janicki <juliusz.janicki@gmail.com>
2024-05-07 13:03:41 +0200

    google-map

--- frontend/src/components/Footer/GoogleMap/GoogleMap.jsx
+++ frontend/src/components/Footer/GoogleMap/GoogleMap.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import css from "./GoogleMap.module.css";
 
 const GoogleMap = () => {
-  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+  // const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
   // Dodaj styl nocny
   const nightMapStyle = [
@@ -30,11 +30,14 @@ const GoogleMap = () => {
 
   return (
     <div className={css.googleMap}>
-      <h2 className={css.googleMap_title}>Lokalizacja punktów obsługi YesNet</h2>
+      <h2 className={css.googleMap_title}>
+        Lokalizacja punktów obsługi YesNet
+      </h2>
       <iframe
         title="Google Map - Lokalizacja biura w Wronkach"
         className={css.mapIframe}
-        src={`https://www.google.com/maps/embed/v1/place?q=UL.%20Towarowa%201%2C%2064-510%20Wronki&key=${apiKey}&maptype=roadmap`}
+        // src={`https://www.google.com/maps/embed/v1/place?q=UL.%20Towarowa%201%2C%2064-510%20Wronki&key=${apiKey}&maptype=roadmap`}
+        src="https://www.google.com/maps/d/u/0/embed?mid=1GmaU2EqRUKf-yCJob2NYPUYUc36CiSc&ehbc=2E312F&noprof=1"
         frameBorder="0"
         style={{ border: 0 }}
         allowFullScreen=""
