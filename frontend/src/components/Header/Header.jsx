f8dacb1480870e80a36db5f502fa2f944814c667
Jonasz Janicki <34692088+PaperManV2@users.noreply.github.com>
2025-05-15 17:42:29 +0200

    cleaning Menu/Header components
    The whole screen seams flat (propably the Menu's fault)
    
    change the value of height in header and scale up the object inside

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,66 +1,45 @@
-import React, { useEffect, useState } from "react";
-import css from "./Header.module.css";
-import icon from "../../icons/icons.svg";
-import Menu from "../Menu/Menu";
+import React, { useEffect, useState } from 'react'
+import css from './Header.module.css'
+import icon from '../../icons/icons.svg'
+import Menu from '../Menu/Menu'
 
 const Header = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const [scroll, setScroll] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false)
 
-  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  window.matchMedia('(min-width: 768px)').addEventListener('change', (e) => {
     if (!e.matches) {
-      return;
+      return
     }
-    setMenuOpen(false);
-  });
+    setMenuOpen(false)
+  })
 
   const toTop = () => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
   }
 
   const hideMenu = () => {
-    setMenuOpen(false);
-  };
+    setMenuOpen(false)
+  }
 
   const toggleMenu = (e) => {
-    e.stopPropagation();
-    if (!window.matchMedia("(min-width: 768px)").matches) {
-      setMenuOpen(!isMenuOpen);
+    e.stopPropagation()
+    if (!window.matchMedia('(min-width: 768px)').matches) {
+      setMenuOpen(!isMenuOpen)
     }
-  };
+  }
 
   useEffect(() => {
     if (isMenuOpen) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = 'hidden'
     } else {
-      document.body.style.overflow = "auto";
+      document.body.style.overflow = 'auto'
     }
-  }, [isMenuOpen]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 1) {
-        setScroll(true);
-      } else {
-        setScroll(false);
-      }
-    };
-    handleScroll();
-    window.addEventListener("scroll", handleScroll);
-
-    // Cleanup function to remove the event listener when component unmounts
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-
+  }, [isMenuOpen])
 
   return (
-    <div className={`${css.header} ${scroll ? css.scroll : ""}`}>
+    <div className={`${css.header}`}>
       <div className={css.logo_icon_container}>
-        <svg className={`${css.logo_icon}`}
-         onClick={toTop}>
+        <svg className={`${css.logo_icon}`} onClick={toTop}>
           <use href={`${icon}#logo`}></use>
         </svg>
       </div>
@@ -86,7 +65,7 @@ const Header = () => {
         </div>
       }
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,110 +1,81 @@
-import css from "./Menu.module.css";
-import icon from "../../icons/icons.svg";
-import olx from "../../icons/olx.svg";
-import { useState, useRef, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import css from './Menu.module.css'
+import icon from '../../icons/icons.svg'
+import olx from '../../icons/olx.svg'
+import { useState, useRef, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Menu = ({ open, onClose, outsideClick }) => {
-  const menuRef = useRef(null);
-  const dialogWindowRef = useRef(null);
-  let { state } = useLocation();
-  let { target } = state || {};
+  let { state } = useLocation()
+  let { target } = state || {}
 
-  const [isCompanyOpen, setIsCompanyOpen] = useState(false);
-  const [isSupportOpen, setIsSupportOpen] = useState(false);
+  const [isCompanyOpen, setIsCompanyOpen] = useState(false)
+  const [isSupportOpen, setIsSupportOpen] = useState(false)
 
   const scrollToSection = (section, offset) => {
-    const sectionToScrollTo = document.getElementById(`${section}`);
+    const sectionToScrollTo = document.getElementById(`${section}`)
 
     if (
-      window.matchMedia("(min-width: 320px)").matches &&
-      window.matchMedia("(max-width: 767px)").matches
+      window.matchMedia('(min-width: 320px)').matches &&
+      window.matchMedia('(max-width: 767px)').matches
     ) {
       window.scrollTo({
         top: sectionToScrollTo.offsetTop - offset[0],
         left: 0,
-        behavior: "smooth",
-      });
+        behavior: 'smooth'
+      })
     } else if (
-      window.matchMedia("(min-width: 768px)").matches &&
-      window.matchMedia("(max-width: 1279px)").matches
+      window.matchMedia('(min-width: 768px)').matches &&
+      window.matchMedia('(max-width: 1279px)').matches
     ) {
-      console.log(sectionToScrollTo.offsetTop, offset[1]);
+      console.log(sectionToScrollTo.offsetTop, offset[1])
       window.scrollTo({
         top: sectionToScrollTo.offsetTop - offset[1],
         left: 0,
-        behavior: "smooth",
-      });
-    } else if (window.matchMedia("(min-width: 1280px)").matches) {
-      console.log(sectionToScrollTo.offsetTop, offset[2]);
+        behavior: 'smooth'
+      })
+    } else if (window.matchMedia('(min-width: 1280px)').matches) {
+      console.log(sectionToScrollTo.offsetTop, offset[2])
       window.scrollTo({
         top: sectionToScrollTo.offsetTop - offset[2],
         left: 0,
-        behavior: "smooth",
-      });
+        behavior: 'smooth'
+      })
     }
-  };
+  }
 
   const toggleCompany = (e) => {
-    e.stopPropagation();
-    setIsCompanyOpen(!isCompanyOpen);
-    setIsSupportOpen(false);
-  };
+    e.stopPropagation()
+    setIsCompanyOpen(!isCompanyOpen)
+    setIsSupportOpen(false)
+  }
 
   const toggleSupport = (e) => {
-    e.stopPropagation();
-    setIsSupportOpen(!isSupportOpen);
-    setIsCompanyOpen(false);
-  };
+    e.stopPropagation()
+    setIsSupportOpen(!isSupportOpen)
+    setIsCompanyOpen(false)
+  }
 
   const hideMenuWindows = () => {
-    setIsCompanyOpen(false);
-    setIsSupportOpen(false);
-  };
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (
-        (menuRef.current && !menuRef.current.contains(event.target)) ||
-        (dialogWindowRef.current &&
-          !dialogWindowRef.current.contains(event.target))
-      ) {
-        outsideClick();
-        hideMenuWindows();
-      }
-    }
-
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, [outsideClick]);
+    setIsCompanyOpen(false)
+    setIsSupportOpen(false)
+  }
 
   useEffect(() => {
     if (target) {
-      scrollToSection(target[0], target[1]);
-      window.history.replaceState({}, "");
+      scrollToSection(target[0], target[1])
+      window.history.replaceState({}, '')
     } else {
-      window.scrollTo(0, 0);
+      window.scrollTo(0, 0)
     }
-  }, [target]);
+  }, [target])
 
   return (
     <>
       {
         <>
-          <div
-            className={`${css.menu} ${open ? css.open : css.close}`}
-            ref={open ? menuRef : null}
-          >
+          <div className={`${css.menu} ${open ? css.open : css.close}`}>
             <div className={css.menuHeader}>
-              <svg
-                className={`${css.arrow_right}`}
-                width="36px"
-                height="32px"
-                fill="white"
-                onClick={onClose}
-              >
+              <svg className={`${css.arrow_right}`} onClick={onClose}>
                 <use href={`${icon}#arrow_right`}></use>
               </svg>
               <div className={css.companyInfo}>
@@ -119,286 +90,109 @@ const Menu = ({ open, onClose, outsideClick }) => {
 
             <ul className={css.navigation}>
               <li className={css.navigation_item}>
-                {/* eslint-disable-next-line */}
                 <a
-                  href="#Header"
                   onClick={outsideClick}
                   className={css.navigation_link}
+                  href='https://ipot24.yesnet.pl'
                 >
-                  <svg
-                    className={`${css.house}`}
-                    width="48px"
-                    height="32px"
-                    fill="white"
-                  >
-                    <use href={`${icon}#house`}></use>
-                  </svg>
-                  <span>Home</span>
-                </a>
-              </li>
-              <li className={css.navigation_item}>
-                {/* eslint-disable-next-line */}
-                <a onClick={outsideClick} className={css.navigation_link}>
-                  <svg
-                    className={`${css.account}`}
-                    width="48px"
-                    height="32px"
-                    fill="white"
-                  >
+                  <svg className={`${css.account}`}>
                     <use href={`${icon}#account`}></use>
                   </svg>
                   <span>Login</span>
                 </a>
               </li>
-              <li className={css.navigation_item} onClick={toggleCompany}>
-                <div className={css.company_container}>
-                  <span
-                    className={`${css.company_title} ${
-                      isCompanyOpen ? css.company_title_open : ""
-                    }`}
-                  >
-                    Company
-                  </span>
-                  <span
-                    className={`${css.company_open_btn} ${
-                      isCompanyOpen ? css.company_open : ""
-                    }`}
-                  >
-                    &#9660;
-                  </span>
-
-                  <ul
-                    className={`${css.company_navigation} ${
-                      isCompanyOpen ? css.company_navigation_open : ""
-                    }`}
-                    ref={isCompanyOpen ? dialogWindowRef : null}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                    }}
-                  >
-                    <li className={css.company_navigation_main_section}>
-                      {/* eslint-disable-next-line */}
-                      <a
-                        onClick={() => {
-                          hideMenuWindows();
-                          outsideClick();
-                          scrollToSection("Ourself", [60, 80, 180]);
-                        }}
-                      >
-                        <svg className={`${css.groupPeople}`} fill="white">
-                          <use href={`${icon}#groupPeople`}></use>
-                        </svg>
-                        <div>
-                          <h4 className={css.company_navigation_item_title}>
-                            O nas
-                          </h4>
-                          <span>Dowiedz się więcej!</span>
-                        </div>
-                      </a>
-                      {/* eslint-disable-next-line */}
-                      <a
-                        onClick={() => {
-                          hideMenuWindows();
-                          outsideClick();
-                          scrollToSection("Footer", [-1430, -1230, -1550]);
-                        }}
-                      >
-                        <svg className={`${css.map}`} fill="white">
-                          <use href={`${icon}#map`}></use>
-                        </svg>
-                        <div>
-                          <h4 className={css.company_navigation_item_title}>
-                            Lokalizacja
-                          </h4>
-                          <span>Gdzie jesteśmy?</span>
-                        </div>
-                      </a>
-                    </li>
-                    <li className={css.company_navigation_section_break}></li>
-                    <li className={css.company_navigation_aside_section}>
-                      {/* eslint-disable-next-line */}
-                      <a
-                        onClick={() => {
-                          hideMenuWindows();
-                          outsideClick();
-                        }}
-                        href="tel:+48690860520"
-                      >
-                        <svg className={`${css.phone}`} fill="white">
-                          <use href={`${icon}#phone`}></use>
-                        </svg>
-                        <div>
-                          <h4>+48690860520</h4>
-                        </div>
-                      </a>
-                      {/* eslint-disable-next-line */}
-                      <a
-                        onClick={() => {
-                          hideMenuWindows();
-                          outsideClick();
-                        }}
-                        href="mailto:bok@yesnet.pl"
-                      >
-                        <svg className={`${css.envelop}`} fill="white">
-                          <use href={`${icon}#envelop`}></use>
-                        </svg>
-                        <div>
-                          <h4>bok@yesnet.pl</h4>
-                        </div>
-                      </a>
-                      <a
-                        onClick={onClose}
-                        href="https://www.olx.pl/oferty/uzytkownik/1UO84R/"
-                      >
-                        <img src={olx} width={24} height={24} alt="olx"></img>
-                        <div>
-                          <h4 className={css.contact_context}>Wyprzedaż</h4>
-                        </div>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+              <li className={css.navigation_item}>
+                <a
+                  className={css.navigation_link}
+                  onClick={() => {
+                    outsideClick()
+                    scrollToSection('Ourself', [60, 80, 180])
+                  }}
+                >
+                  <svg className={`${css.groupPeople}`}>
+                    <use href={`${icon}#groupPeople`}></use>
+                  </svg>
+                  <span>O nas</span>
+                </a>
               </li>
               <li className={css.navigation_item}>
-                <div className={css.support_container} onClick={toggleSupport}>
-                  <span
-                    className={`${css.support_title} ${
-                      isSupportOpen ? css.support_title_open : ""
-                    }`}
-                  >
-                    Support
-                  </span>
-                  <span
-                    className={`${css.support_open_btn} ${
-                      isSupportOpen ? css.support_open : ""
-                    }`}
-                  >
-                    &#9660;
-                  </span>
+                <a
+                  className={css.navigation_link}
+                  onClick={() => {
+                    outsideClick()
+                    scrollToSection('Footer', [-1430, -1230, -1550])
+                  }}
+                >
+                  <svg className={`${css.map}`}>
+                    <use href={`${icon}#map`}></use>
+                  </svg>
 
-                  <ul
-                    className={`${css.support_navigation} ${
-                      isSupportOpen ? css.suppport_navigation_open : ""
-                    }`}
-                    ref={isSupportOpen ? dialogWindowRef : null}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                    }}
-                  >
-                    <li>
-                      {/* eslint-disable-next-line */}
-                      <a
-                        onClick={() => {
-                          hideMenuWindows();
-                          outsideClick();
-                          scrollToSection("News", [110, 150, 220]);
-                        }}
-                      >
-                        <svg
-                          className={`${css.news}`}
-                          width="20%"
-                          height="50%"
-                          fill="white"
-                        >
-                          <use href={`${icon}#news`}></use>
-                        </svg>
-                        <div>
-                          <h4 className={css.company_navigation_item_title}>
-                            Aktualności
-                          </h4>
-                          <span>Co nowego!</span>
-                        </div>
-                      </a>
-                    </li>
-                    <li>
-                      {/* eslint-disable-next-line */}
-                      <a
-                        onClick={() => {
-                          hideMenuWindows();
-                          outsideClick();
-                          scrollToSection("Footer", [-730, -600, -610]);
-                        }}
-                      >
-                        <svg
-                          className={`${css.pen}`}
-                          width="20%"
-                          height="50%"
-                          fill="white"
-                        >
-                          <use href={`${icon}#pen`}></use>
-                        </svg>
-                        <div>
-                          <h4 className={css.company_navigation_item_title}>
-                            Zgłoszenia
-                          </h4>
-                          <span>W czym problem?</span>
-                        </div>
-                      </a>
-                    </li>
-                    <li>
-                      {/* eslint-disable-next-line */}
-                      <a
-                        onClick={() => {
-                          hideMenuWindows();
-                          outsideClick();
-                          scrollToSection("MiniFaq", [80, 80, 140]);
-                        }}
-                      >
-                        <svg
-                          className={`${css.faq}`}
-                          width="20%"
-                          height="50%"
-                          fill="white"
-                        >
-                          <use href={`${icon}#faq`}></use>
-                        </svg>
-                        <div>
-                          <h4 className={css.company_navigation_item_title}>
-                            FAQ
-                          </h4>
-                          <span>Znajdź rozwiązanie</span>
-                        </div>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+                  <span>Lokalizacja</span>
+                </a>
+              </li>
+              <li className={css.navigation_item}>
+                <a
+                  className={css.navigation_link}
+                  onClick={() => {
+                    outsideClick()
+                  }}
+                  href='tel:+48690860520'
+                >
+                  <svg className={`${css.phone}`}>
+                    <use href={`${icon}#phone`}></use>
+                  </svg>
+                  <span>Kontakt</span>
+                </a>
+              </li>
+              <li className={css.navigation_item}>
+                <a
+                  className={css.navigation_link}
+                  onClick={() => {
+                    outsideClick()
+                    scrollToSection('News', [110, 150, 220])
+                  }}
+                >
+                  <svg className={`${css.news}`}>
+                    <use href={`${icon}#news`}></use>
+                  </svg>
+
+                  <span>Aktualności</span>
+                </a>
+              </li>
+              <li className={css.navigation_item}>
+                <a
+                  className={css.navigation_link}
+                  onClick={() => {
+                    outsideClick()
+                    scrollToSection('Footer', [-730, -600, -610])
+                  }}
+                >
+                  <svg className={`${css.pen}`}>
+                    <use href={`${icon}#pen`}></use>
+                  </svg>
+                  <span>Zgłoszenia</span>
+                </a>
+              </li>
+              <li className={css.navigation_item}>
+                <a
+                  className={css.navigation_link}
+                  onClick={() => {
+                    outsideClick()
+                  }}
+                  href='/faq'
+                >
+                  <svg className={`${css.faq}`}>
+                    <use href={`${icon}#faq`}></use>
+                  </svg>
+                  <span>FAQ</span>
+                </a>
               </li>
             </ul>
-            <div>
-              <a
-                className={css.olx}
-                onClick={onClose}
-                href="https://www.olx.pl/oferty/uzytkownik/1UO84R/"
-              >
-                <img src={olx} width={24} height={24} alt="olx"></img>
-
-                <h4 className={css.contact_context}>Wyprzedaż</h4>
-              </a>
-            </div>
-            <div className={css.mobile_menu_contact_section}>
-              <a onClick={onClose} href="tel:+48690860520">
-                <svg className={`${css.phone}`} fill="white">
-                  <use href={`${icon}#phone`}></use>
-                </svg>
-                <div>
-                  <h4 className={css.contact_context}>+48690860520</h4>
-                </div>
-              </a>
-
-              <a onClick={onClose} href="mailto:bok@yesnet.pl">
-                <svg className={`${css.envelop}`} fill="white">
-                  <use href={`${icon}#envelop`}></use>
-                </svg>
-                <div>
-                  <h4 className={css.contact_context}>bok@yesnet.pl</h4>
-                </div>
-              </a>
-            </div>
           </div>
         </>
       }
     </>
-  );
-};
+  )
+}
 
-export default Menu;
+export default Menu
