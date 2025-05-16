dd55094237067eea83a95b3c807291f18ee164cc
K4pe7u <juliusz.janicki@gmail.com>
2025-05-16 14:56:54 +0200

    fjut

--- frontend/src/components/Mainset/Mainset.jsx
+++ frontend/src/components/Mainset/Mainset.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import css from "./Mainset.module.css";
-
-// eslint-disable-next-line
-import desktoplogo from "../../image/logo1440x2070.png";
+import desktoplogo from "../../image/YesNET.png";
 
 const Mainset = ({open}) => {
   return (
     <div className={css.sellContainer}>
-      <div className={css.corectionBox}>
-        <div className={css.titleContainer}>
-          <img src={desktoplogo} className={css.logoImage} alt="Desktop"/>
-        </div>
+      <div className={css.titleContainer}>
+        <img src={desktoplogo} className={css.logoImage} alt="YesNET Logo" />
       </div>
       
       <div className={css.joinContainer}>
         <div className={css.solutionSecondary}>
-          Skrój Pakiet Dla Swoich Potrzeb!
+          Skrój Internet dla Swoich potrzeb!
         </div>
         <div className={css.optionContainer}>
           <button
@@ -24,16 +20,16 @@ const Mainset = ({open}) => {
             className={css.individualOption}
           >
             <span className={css.highlight2}></span>
-            <span className={css.buttonText2}>Rozwiązania indywidualne</span>
+            <span className={css.buttonText2}>Złóż zapytanie</span>
           </button>
-          <button
+          {/* <button
             onClick={() => open("business")}
             type="button"
             className={css.companyOption}
           >
             <span className={css.highlight}></span>
             <span className={css.buttonText}>Rozwiązania biznesowe</span>
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
